c3aa722578d39bba516d20208f74d8bde343c0b4
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-08-25 00:56:14 +0900

    modifying

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
             flexDirection: 'column', alignItems: 'center',
             justifyContent: 'center', fontSize:'1rem'
         }}>
-            <p> React With Node <Icon type="smile" /></p>
+            <p> DBLAB_PROJECT <Icon type="smile" /></p>
         </div>
     )
 }
