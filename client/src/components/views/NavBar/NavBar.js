35e5d00ef8b1093246e16963fd552e0e51623a24
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-08-30 22:28:39 +0900

    image insert...

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -17,11 +17,10 @@ function NavBar() {
   };
 
   return (
-    <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
+    <nav className="menu" style={{ position: 'static', zIndex: 5, width: '100%' }}>
       <div className="menu__logo">
         <a href="/">
-          {/* <img src={mainlogo}/> */}
-          mainlogo
+            <img src={mainlogo}/>
         </a>
       </div>
       <div className="menu__container">
