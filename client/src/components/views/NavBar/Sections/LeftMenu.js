5143801afbcde3b6b4e4263c3337ab4022363b98
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-09-03 00:01:44 +0900

    modifying

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -10,7 +10,7 @@ function LeftMenu(props) {
         <a href="/">Home</a>
       </Menu.Item>
       <Menu.Item key="favorite">
-        <a href="/favorite">Favorite</a>
+        <a href="/favorite">★ Favorite</a>
       </Menu.Item>
     </Menu>
   )
