10132cd1c82fe28ef0674f90dd935826d097c8fe
OHYUNBEOM <dbsqja353@naver.com>
2021-09-07 01:30:36 +0900

    modifying

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -7,26 +7,14 @@ function GridCards(props) {
         return (
             <Col lg={6} md={8} xs={24}>{/* 총 크기가 24인데, 전체화면일때는 6으로 즉 4칸, 줄어들면 8으로 즉 3칸, 제일작을때는 24 즉 한칸만 */}
                 <div style={{ position: 'relative' }}>
-                    <a href={`/movie/${props.movieId}`} >
-                        <h2>등록된 메뉴</h2>
-                        {/* <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName} /> */}
+                    <a href={`/menu/${props.menuId}`} >
+                        <h2>Registered Menus</h2>
+                        {/* <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.menuName} /> */}
                     </a>
                 </div>
             </Col>
         )
-    } else {// Actors image
-        return (
-            <Col lg={6} md={8} xs={24}>
-                <div style={{ position: 'relative' }}>
-
-                    <h2>원재료</h2>
-                    {/* <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.characterName} /> */}
-
-                </div>
-            </Col>
-        )
-    }
-
+    } 
 }
 
 export default GridCards
