7373cb1aaf2957635e57516171867c540e40c661
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-09-06 15:28:10 +0900

    modifying

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -8,7 +8,7 @@ function GridCards(props) {
             <Col lg={6} md={8} xs={24}>{/* 총 크기가 24인데, 전체화면일때는 6으로 즉 4칸, 줄어들면 8으로 즉 3칸, 제일작을때는 24 즉 한칸만 */}
                 <div style={{ position: 'relative' }}>
                     <a href={`/movie/${props.movieId}`} >
-                        <h2>Registered Menus</h2>
+                        <h2>등록된 메뉴</h2>
                         {/* <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName} /> */}
                     </a>
                 </div>
@@ -19,7 +19,7 @@ function GridCards(props) {
             <Col lg={6} md={8} xs={24}>
                 <div style={{ position: 'relative' }}>
 
-                    <h2>Registered ingredients</h2>
+                    <h2>원재료</h2>
                     {/* <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.characterName} /> */}
 
                 </div>
